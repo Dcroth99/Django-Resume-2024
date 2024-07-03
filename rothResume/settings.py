d7e4cf3d3f640669d49fb2a47f59580be7b506f7
Daniel Roth <danielcharlesroth@gmail.com>
2024-07-03 13:18:45 -1000

    false1

--- rothResume/settings.py
+++ rothResume/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$prbo7u*9#k-5o@bpqk(3s4e5=vg%m0i4##mhch%t_7pqzbws9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['drothresumeapp.herokuapp.com', 'localhost', '127.0.0.1']
 
